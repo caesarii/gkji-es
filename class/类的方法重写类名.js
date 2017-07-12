eff477041d3a0d026b8ea7c215f2122112a12618
qinghe <qinghe@2dfire.com>
2017-07-12 14:03:47 +0800

    object and class

---
+++ class/类的方法重写类名.js
@@ -0,0 +1,11 @@
+
+class Person {
+
+    Person() {
+        console.log('hi', this.name)
+    }
+
+}
+
+const p = new Person()
+p.Person()
---
+++ object/实例中获取原型-super.js
@@ -0,0 +1,35 @@
+
+const person = {
+    name: 'person',
+    greeting() {
+        return `hello, ${this.name}`
+    }
+}
+
+const dog = {
+    name: 'student',
+    greeting() {
+        return `wooff, ${this.name}`
+    }
+}
+
+// 根据原型，调用同名方法
+let friend = {
+    name: 'qinghe',
+    greeting() {
+        // Object.getPrototypeOf 与 .constructor.prototype 的区别 ？
+        // 对象字面量的.constructor 是 Object
+        // return this.constructor.prototype.greeting.call(this)
+
+        // console.log(Object.getPrototypeOf(this))
+        // return Object.getPrototypeOf(this).greeting.call(this)
+        return super.greeting()
+    }
+}
+
+Object.setPrototypeOf(friend, person)
+console.log(friend.greeting())
+
+Object.setPrototypeOf(friend, dog)
+console.log(friend.greeting())
+
